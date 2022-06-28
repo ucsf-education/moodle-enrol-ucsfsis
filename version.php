3952da541d53e3a37b89a8201fe803c0a2e22723
Carson Tam <carson.tam@ucsf.edu>
2022-06-28 14:39:22 -0700

    Updated version.php.

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021032501;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2022062800;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200;        // Requires this Moodle version
 $plugin->component = 'enrol_ucsfsis';   // Full name of the plugin (used for diagnostics)
